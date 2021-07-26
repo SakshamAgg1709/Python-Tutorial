19843042f0056956648e497096909e331aa0cd44
Saksham <sabusiness1709@gmail.com>
2021-07-26 16:51:02 +0530

    Chapter1 Practise set is added

---
+++ Python tuts2 - practise/Chapter1.py
@@ -0,0 +1,24 @@
+#Practise set
+
+"""Problem 1"""
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky""") # Or we can use '''
+#Ek se zyada line stand karne ale strig ko """ isme ya phir ''' isme karte h similar to comments
+
+"""Next Problem - Print table of 5 in REPl or any terminal - cmd or Windows Powershell"""
+
+"""Problem 3 - playing a mp3 using a external module"""
+
+from playsound import playsound
+
+# playsound('C:\\Users\\Admin\\Python\\PycharmProjects\\PythonTuts\\water.mp3')
+# Writing \\ is important otherwise it will treat it as a escape sequence
+
+"""Problem 4 - listing the path of your file using os"""
+import os
+
+print(os.listdir())
+
+"""Thats it """
