1529237f8a2aef476bee139a031f96573969ffac
Saksham <sabusiness1709@gmail.com>
2021-08-07 13:41:32 +0530

    "Chapter 3 Practise -Set"

---
+++ Python tuts2 - practise/Chapter2..py
@@ -0,0 +1,27 @@
+#cHAPTER 2 PRACTISE SET
+
+a = 55
+b =5
+
+print("The sum of and b is" , a +b)
+
+c =44
+d = 5
+
+print("THe remainder when c is divided by d is" , c%d)
+
+inp = input("Enter any thing")
+print(type(inp))
+
+a = 34
+b = 80
+
+print(a>=b)
+
+a =int(input("Enter the first number:\n"))
+b =int(input("Enter the second number:\n"))
+
+print("The average is" , (a+b)/2)
+
+a = int(input("Enter any number"))
+print("Square of entered number is" , a**a)
